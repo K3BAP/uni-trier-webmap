fc4cc63f3bbc3d02d15343e52d4dd6d89522bdf8
Fabian Sponholz <s4faspon@uni-trier.de>
2023-07-21 12:00:28 +0200

    Cleanup, integrate map into app for convenience

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      dense
-      color="primary"
-      dark
-    >
+    <v-app-bar app dense color="primary" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Campuskarte Uni Trier</v-toolbar-title>
@@ -13,93 +8,93 @@
       <v-spacer></v-spacer>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
           <h1>Layer-Optionen</h1>
           <v-divider class="my-2"></v-divider>
           <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                dark
-                v-bind="attrs"
-                v-on="on"
-              >
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
                 Hinzufügen
               </v-btn>
             </template>
             <v-list>
-              <v-list-item
-                v-for="layer in availableLayers"
-                :key="availableLayers.indexOf(layer)"
-              >
+              <v-list-item v-for="layer in availableLayers" :key="availableLayers.indexOf(layer)">
                 <v-list-item-title>{{ layer.title }}</v-list-item-title>
               </v-list-item>
             </v-list>
           </v-menu>
-          <LayerOptions v-for="layer in activeLayers" :layer-container="availableLayers[layer]" :key="availableLayers.indexOf(layer)"/>
+          <LayerOptions v-for="layer in activeLayers" :layer-container="availableLayers[layer]"
+            :key="availableLayers.indexOf(layer)" />
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
 
 
     <v-main>
-      <MapView/>
+      <div id="map"></div>
     </v-main>
     <v-footer padless class="d-none d-lg-block">
-    <v-col
-      class="text-center"
-      cols="12"
-    >
-      {{ new Date().getFullYear() }} — <strong>Fabian Sponholz</strong> (Projekt Seminar Kartographische Informatik)
-    </v-col>
-  </v-footer>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Fabian Sponholz</strong> (Projekt Seminar Kartographische Informatik)
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import MapView from './components/Map.vue';
+import { Map, View } from 'ol';
 import LayerOptions from './components/LayerOptions.vue';
-import { defineLayerContainer } from './assets/js/defineLayerContainer';
+import { defineLayerContainer, defineBaseLayers } from './assets/js/defineLayerContainer';
+
+const baseLayers = defineBaseLayers()
 
 export default {
   name: 'App',
 
   components: {
-    MapView,
     LayerOptions
-},
+  },
 
   data: () => ({
     drawer: false,
     group: null,
+    map: null,
 
     availableLayers: defineLayerContainer(),
-    activeLayers:[
+    activeLayers: [
       0,
     ]
   }),
+  mounted() {
+    console.log("creating map");
+    this.map = new Map({
+      target: 'map',
+      layers: baseLayers,
+      view: new View({
+        center: [40818900, 6402500],
+        zoom: 16,
+        minZoom: 14
+      })
+    });
+    // Add initial layers to map
+    this.activeLayers.forEach(layerId => {
+      this.map.addLayer(this.availableLayers[layerId].layer);
+    })
+  },
 
   watch: {
-      group () {
-        //this.drawer = false
-      },
-    },  
+    group() {
+      //this.drawer = false
+    },
+  },
 };
 </script>
 
 <style>
 @import url('../node_modules/ol/ol.css');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -108,8 +103,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
- 
-html, body, #map, #app {
+
+html,
+body,
+#map,
+#app {
   height: 100%;
   margin: 0;
   padding: 0;
