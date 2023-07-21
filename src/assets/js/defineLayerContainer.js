6d81f3fb9d332506aa3b253d251fddfe1f3c26dc
Fabian Sponholz <s4faspon@uni-trier.de>
2023-07-21 23:09:32 +0200

    WFS layer added (Bushaltestellen)

--- src/assets/js/defineLayerContainer.js
+++ src/assets/js/defineLayerContainer.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
-import {Tile} from 'ol/layer';
-import {OSM, Stamen, BingMaps, TileWMS} from 'ol/source';
-
+import { Tile, Vector } from 'ol/layer';
+import { TileWMS, Vector as SourceVector } from 'ol/source';
+import { GeoJSON } from 'ol/format';
+import { Style, Circle, Fill, Stroke } from 'ol/style';
+ 
 /**
  * Define layers to be displayed at all times
  */
-export function defineBaseLayers(){
+export function defineBaseLayers() {
   var layers = [
     // DOP Layer
     new Tile({
@@ -56,6 +58,32 @@ export function defineLayerContainer() {
           params: { 'LAYERS': 'PROJ:fusswege', 'TILED': true },
           serverType: 'geoserver'
         })
+      })
+    },
+    {
+      title: 'Bushaltestellen',
+      hasSettings: true,
+      layer: new Vector({
+        source: new SourceVector({
+          format: new GeoJSON(),
+          url: 'http://localhost:8080/geoserver/wfs?service=WFS&' +
+               'version=1.1.0' +
+               '&request=GetFeature' +
+               '&typename=PROJ:bushaltestellen' +
+               '&outputFormat=application/json'
+        }),
+        style: new Style({
+          image: new Circle({
+               fill: new Fill({
+                color: 'rgb(0, 0, 255)'
+              }),
+               radius: 8,
+               stroke: new Stroke({
+                color: 'rgb(0, 0, 0)',
+                width: 0.8
+              })
+             })
+          })
       })
     },
     {
