b173edbb20faf7253fd083ab1290bc541d6ab1f0
Fabian Sponholz <s4faspon@uni-trier.de>
2023-07-21 21:56:46 +0200

    Prototype popup added

--- src/components/LayerOptions.vue
+++ src/components/LayerOptions.vue
@@ -1,57 +1,42 @@
 <template>
     <v-container>
-        <v-card 
-          class="px-4 pb-5"
-          shaped
-          outlined
-          elevation="5"
-        >
+        <v-card class="px-4 pb-5" shaped outlined elevation="5">
             <v-row class="mt-1 ms-1">
-                <h3>{{layerContainer.title}}</h3>
+                <h3>{{ layerContainer.title }}</h3>
             </v-row>
             <v-row class="">
-                <v-slider
-                v-model="slider"
-                thumb-label
-                min="0"
-                max="100"
-                @change="setOpacity()"
-                @start="locked = true"
-                @end="locked = false"
-                ></v-slider>
+                <v-slider v-model="slider" thumb-label min="0" max="100" @change="setOpacity()" @start="locked = true"
+                    @end="locked = false"></v-slider>
             </v-row>
             <v-row justify="center" class="">
-                <v-btn
-                    class="mx-1"
-                    elevation="2"
-                    fab
-                    x-small
-                    @click="moveUp"
-                ><v-icon> mdi-arrow-up </v-icon></v-btn>
-                <v-btn
-                    class="mx-1"
-                    elevation="2"
-                    fab
-                    x-small
-                    @click="moveDown"
-                ><v-icon> mdi-arrow-down </v-icon></v-btn>
-                <v-btn
-                    v-if="layerContainer.hasSettings"
-                    class="mx-1"
-                    elevation="2"
-                    fab
-                    x-small
-                ><v-icon> mdi-cog </v-icon></v-btn>
-                <v-btn
-                    class="mx-1"
-                    elevation="2"
-                    fab
-                    x-small
-                    @click="deleteLayer"
-                >
-                <v-icon> mdi-close </v-icon></v-btn>
+                <v-btn class="mx-1" elevation="2" fab x-small @click="moveUp"><v-icon> mdi-arrow-up </v-icon></v-btn>
+                <v-btn class="mx-1" elevation="2" fab x-small @click="moveDown"><v-icon> mdi-arrow-down </v-icon></v-btn>
+                <v-btn v-if="layerContainer.hasSettings" class="mx-1" elevation="2" fab x-small @click.stop="dialog=true"><v-icon> mdi-cog </v-icon></v-btn>
+                <v-btn class="mx-1" elevation="2" fab x-small @click="deleteLayer"><v-icon> mdi-close </v-icon></v-btn>
             </v-row>
         </v-card>
+
+        <v-dialog v-model="dialog" max-width="290">
+            <v-card class="rounded-xl">
+                <v-card-title class="text-h5">
+                    Gebäude suchen
+                </v-card-title>
+
+                <v-text-field
+                    class="mx-2"
+                    label="Gebäude"
+                    placeholder="alle"
+                    outlined
+                ></v-text-field>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="green darken-1" text @click="dialog = false">
+                        Anwenden
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 <script>
@@ -63,6 +48,7 @@ export default {
         return {
             slider: 50,
             locked: false,
+            dialog: false,
         }
     },
     methods: {
@@ -76,9 +62,9 @@ export default {
             this.$emit('delete');
         },
         setOpacity() {
-            let opacity = this.slider/100;
+            let opacity = this.slider / 100;
             console.log('setting opacity ' + opacity)
-            this.layerContainer.layer.setOpacity(this.slider/100);
+            this.layerContainer.layer.setOpacity(this.slider / 100);
         },
     },
     mounted() {
